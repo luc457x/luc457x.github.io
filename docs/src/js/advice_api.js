0617b849e72f5fcbb0014c01c6217f2b22345564
luc457x <lucas7eixeir4@outlook.com>
2025-03-19 21:17:22 -0300

    minor changes

--- docs/src/js/advice_api.js
+++ docs/src/js/advice_api.js
@@ -1,6 +1,8 @@
 let adviceDiv = document.querySelector('#adviceDiv');
-let idDiv = document.querySelector('#idDiv')
+let idDiv = document.querySelector('#idDiv');
+let card = document.querySelector('.card');
 
+function fetchAdvice() {
 fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
@@ -9,3 +11,13 @@ fetch('https://api.adviceslip.com/advice')
     .catch(error => {
         console.error('Error:', error);
     });
+}
+
+fetchAdvice();
+
+card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+    if (card.classList.contains('flipped')) {
+        fetchAdvice();
+    }
+});
