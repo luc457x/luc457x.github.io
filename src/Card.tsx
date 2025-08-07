0a6b172d569d499f04ba4ee39f4bf11b0cb76318
luc457x <lucas7eixeir4@outlook.com>
2025-08-07 20:25:35 -0300

    fix

--- src/Card.tsx
+++ src/Card.tsx
@@ -11,21 +11,21 @@ function Card() {
             <div className="card">
                 <div className={`inner ${Flipped ? 'flip' : ''}`} onClick={handleFlip}>
                     <div className="card-front">
-                        <img src="./public/logo.png" alt="logo" />
+                        <img src="/logo.png" alt="logo" />
                         <h1>Lucas Teixeira</h1>
                         <div className="roles">
                             <p>Data Scientist<br />&<br />Dev. Full Stack</p>
                         </div>
                     </div>
                     <div className="card-back">
-                        <img src="./src/img/qr.gif" alt="QR Code" />
+                        <img src="./qr.gif" alt="QR Code" />
                         <div className="contacts">
                             <div className="contact-item">
-                                <img src="./src/img/wpp.png" alt="whatsapp" />
+                                <img src="./wpp.png" alt="whatsapp" />
                                 <a href="https://api.whatsapp.com/send?phone=5522998923719" target="_blank">+55 (22) 99892-3719</a>
                             </div>
                             <div className="contact-item">
-                                <img src="./src/img/e-mail.webp" alt="e-mail" />
+                                <img src="./e-mail.webp" alt="e-mail" />
                                 <a href="mailto:contato@lucas7x.win" target="_blank">contato@lucas7x.win</a>
                             </div>
                         </div>
