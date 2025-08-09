b02731d6b201c7df202ed1e445feebea916d7b62
luc457x <lucas7eixeir4@outlook.com>
2025-08-09 02:28:14 -0300

    working: card

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,39 @@ import './reset.css'
 import './variables.css'
 import './App.css'
 import Card from './Card.tsx'
-import Portfolio from './Portfolio.tsx'
 
 function App() {
-  const [state, setState] = useState(false)
+  const [state, setState] = useState<boolean>(false)
+  const [flipped, setFlipped] = useState<boolean>(false);
+  const handleState = () => {
+    setFlipped(false);
+    setTimeout(() => {
+      setState(!state);
+    }, 600);
+  };
 
   return (
     <>
       <header>
         {state == false && (
-          <a href="#" onClick={() => setState(true)} >Portfolio</a>
+          <a href="#" onClick={handleState} >Portfolio</a>
         )}
         {state == true && (
-          <a href="#" onClick={() => setState(false)} >Home</a>
+          <a href="#" onClick={handleState} >Home</a>
         )}
         <a href="https://www.linkedin.com/in/lucas-de-paula-teixeira-24148a177/" target="_blank">LinkedIn</a>
         <a href="https://linktr.ee/lucas7x" target="_blank">Linktree</a>
       </header>
       <main>
-        {state == false && (
-          <Card />
-        )}
-        {state == true && (
-          <Portfolio />
-        )}
+        <Card
+          flipped={flipped}
+          state={state}
+          setFlipped={setFlipped}
+        />
       </main>
       <footer>
         <div>
-          &copy; 2024 : <a href="https://lucas7x.win" target="_blank">Luc45</a>
+          &#x1F12F; 2024 : <a href="https://lucas7x.win" target="_blank">Luc45</a>
         </div>
       </footer>
     </>
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react';
+import React from 'react';
 
-function Card() {
-    const [Flipped, setFlipped] = useState(false);
-    const handleFlip = () => {
-        setFlipped(!Flipped);
-    };
+interface CardProps {
+        state: boolean;
+        flipped: boolean;
+        setFlipped: React.Dispatch<React.SetStateAction<boolean>>;
+    }
+
+function Card({ state, flipped, setFlipped }: CardProps) {
 
     return (
         <>
-            <div className="card">
-                <div className={`inner ${Flipped ? 'flip' : ''}`} onClick={handleFlip}>
+            <div className={`card ${state ? 'large' : ''}`}>
+                <div className={`inner ${flipped ? 'flip' : ''}`} onClick={() => setFlipped(!flipped)}>
                     <div className="card-front">
-                        <img src="/logo.png" alt="logo" />
-                        <h1>Lucas Teixeira</h1>
-                        <div className="roles">
-                            <p>Data Scientist<br />&<br />Dev. Full Stack</p>
-                        </div>
+                        {state == false ? (
+                            <>
+                                <img src="/logo.png" alt="logo" />
+                                <h1>Lucas Teixeira</h1>
+                                <div className="roles">
+                                    <p>Data Scientist<br />&<br />Dev. Full Stack</p>
+                                </div>
+                            </>
+                        ): null}                        
                     </div>
                     <div className="card-back">
-                        <img src="./qr.gif" alt="QR Code" />
-                        <div className="contacts">
-                            <div className="contact-item">
-                                <img src="./wpp.png" alt="whatsapp" />
-                                <a href="https://api.whatsapp.com/send?phone=5522998923719" target="_blank">+55 (22) 99892-3719</a>
-                            </div>
-                            <div className="contact-item">
+                        {state == false ? (
+                            <>
+                                <img src="./qr.gif" alt="QR Code" />
+                                <div className="contacts">
+                                    <div className="contact-item">
+                                        <img src="./wpp.png" alt="whatsapp" />
+                                        <a href="https://api.whatsapp.com/send?phone=5522998923719" target="_blank">+55 (22) 99892-3719</a>
+                                    </div>
+                                <div className="contact-item">
                                 <img src="./e-mail.webp" alt="e-mail" />
                                 <a href="mailto:contato@lucas7x.win" target="_blank">contato@lucas7x.win</a>
                             </div>
                         </div>
+                            </>
+                        ): null} 
                     </div>
                 </div>
             </div>
